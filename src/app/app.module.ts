e6ed5f73b253e43952c7b7a2f5efb09e5f1ca41a
YoussefHissi <youssefhissi789@gmail.com>
2023-06-26 23:54:02 +0200

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './components/home/home.component';
+import { SigninComponent } from './components/signin/signin.component';
+import { SignupComponent } from './components/signup/signup.component';
+import { WatchComponent } from './components/watch/watch.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    SigninComponent,
+    SignupComponent,
+    WatchComponent
   ],
   imports: [
     BrowserModule,
