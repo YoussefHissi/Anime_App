7dc9660ff14559582c1331cfc28e0836ed336571
YoussefHissi <youssefhissi789@gmail.com>
2023-06-27 12:46:38 +0200

    add the routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './components/home/home.component';
+import { SigninComponent } from './components/signin/signin.component';
+import { SignupComponent } from './components/signup/signup.component';
+import { WatchComponent } from './components/watch/watch.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:"home", component:HomeComponent},
+  {path:"", redirectTo:"home",pathMatch:"full"},
+  {path:"signin", component:SigninComponent},
+  {path:"signup", component:SignupComponent},
+  {path:"watch", component:WatchComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
